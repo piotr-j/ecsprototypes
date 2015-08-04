3eedf7b4a961afe130b2efd789587579c6565fc1
PiotrJ <me@piotrjastrzebski.io>
2015-08-04 10:27:09 +0200

    added roguer

--- core/src/io/piotrjastrzebski/ecsclones/roguer/Roguer.java
+++ core/src/io/piotrjastrzebski/ecsclones/roguer/Roguer.java
@@ -1,8 +1,14 @@
 package io.piotrjastrzebski.ecsclones.roguer;
 
+import io.piotrjastrzebski.ecsclones.ECSGame;
+import io.piotrjastrzebski.ecsclones.base.GameScreen;
+
 /**
  * Simple rogue like in ecs
  * Created by EvilEntity on 30/07/2015.
  */
-public class Roguer {
+public class Roguer extends GameScreen {
+	public Roguer (ECSGame game) {
+		super(game);
+	}
 }
