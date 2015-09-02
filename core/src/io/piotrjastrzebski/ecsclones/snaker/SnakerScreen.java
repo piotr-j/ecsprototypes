71787eeb099a2643cec6b71cc301c7b98df3e347
PiotrJ <me@piotrjastrzebski.io>
2015-09-02 12:19:21 +0200

    added snaker stub

---
+++ core/src/io/piotrjastrzebski/ecsclones/snaker/SnakerScreen.java
@@ -0,0 +1,24 @@
+package io.piotrjastrzebski.ecsclones.snaker;
+
+import com.artemis.WorldConfiguration;
+import io.piotrjastrzebski.ecsclones.ECSGame;
+import io.piotrjastrzebski.ecsclones.base.GameScreen;
+
+/**
+ * Simple angry birds like in ecs
+ * Created by EvilEntity on 30/07/2015.
+ */
+public class SnakerScreen extends GameScreen {
+
+	public SnakerScreen (ECSGame game) {
+		super(game);
+	}
+
+	@Override protected void preInit (WorldConfiguration config) {
+
+	}
+
+	@Override protected void postInit () {
+
+	}
+}
