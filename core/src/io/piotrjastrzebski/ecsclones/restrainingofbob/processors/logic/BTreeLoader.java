856684a9c028640fd8c1ce521760d6885bfe8dd5
PiotrJ <me@piotrjastrzebski.io>
2015-09-28 21:09:42 +0200

    trees are now pooled

--- core/src/io/piotrjastrzebski/ecsclones/restrainingofbob/processors/logic/BTreeLoader.java
+++ core/src/io/piotrjastrzebski/ecsclones/restrainingofbob/processors/logic/BTreeLoader.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.ai.btree.LeafTask;
 import com.badlogic.gdx.ai.btree.Task;
 import com.badlogic.gdx.ai.btree.utils.BehaviorTreeParser;
 import com.badlogic.gdx.utils.ObjectMap;
+import com.badlogic.gdx.utils.Pool;
 import com.badlogic.gdx.utils.StreamUtils;
 import io.piotrjastrzebski.ecsclones.restrainingofbob.components.logic.EnemyBTree;
 import io.piotrjastrzebski.ecsclones.restrainingofbob.components.logic.EnemyBrain;
@@ -22,26 +23,51 @@ import java.io.Reader;
 public class BTreeLoader extends EntitySystem {
 	private final static String TAG = BTreeLoader.class.getSimpleName();
 	protected ComponentMapper<EnemyBrain> mEnemyBrain;
+	protected ComponentMapper<EnemyBTree> mEnemyBTree;
 
 	public BTreeLoader () {
 		super(Aspect.all(EnemyBrain.class));
 		setPassive(true);
 	}
 
-	@Override protected void initialize () {}
+	ObjectMap<String, Pool<BehaviorTree<EnemyBrain>>> bTreePools = new ObjectMap<>();
+
+	@Override protected void initialize () {
+	}
 
 	@Override protected void inserted (int eid) {
 		EnemyBrain brain = mEnemyBrain.get(eid);
 		EnemyBTree tree = world.getEntity(eid).edit().create(EnemyBTree.class);
-		tree.tree = (BehaviorTree<EnemyBrain>)get(brain.treePath).cloneTask();
+		tree.set(brain.treePath, get(brain.treePath));
 	}
 
-	ObjectMap<String, BehaviorTree<EnemyBrain>> trees = new ObjectMap<>();
-	private BehaviorTree<EnemyBrain> get (String path) {
-		BehaviorTree<EnemyBrain> tree = trees.get(path);
+	ObjectMap<String, BehaviorTree<EnemyBrain>> archetypes = new ObjectMap<>();
+
+	private BehaviorTree<EnemyBrain> get (final String path) {
+		Pool<BehaviorTree<EnemyBrain>> pool = bTreePools.get(path, null);
+		if (pool == null) {
+			pool = new Pool<BehaviorTree<EnemyBrain>>() {
+				@Override protected BehaviorTree<EnemyBrain> newObject () {
+					return (BehaviorTree<EnemyBrain>)getArchetype(path).cloneTask();
+				}
+			};
+			bTreePools.put(path, pool);
+		}
+		return pool.obtain();
+	}
+
+	@Override protected void removed (int entityId) {
+		EnemyBTree tree = mEnemyBTree.get(entityId);
+		BehaviorTree<EnemyBrain> bt = tree.tree;
+		bt.reset();
+		bTreePools.get(tree.path).free(bt);
+	}
+
+	private BehaviorTree<EnemyBrain> getArchetype (String path) {
+		BehaviorTree<EnemyBrain> tree = archetypes.get(path, null);
 		if (tree == null) {
 			tree = load(path);
-			trees.put(path, tree);
+			archetypes.put(path, tree);
 		}
 		return tree;
 	}
