0fcdc8f40804ec68498d11328d46fab203641d5e
PiotrJ <me@piotrjastrzebski.io>
2015-09-29 16:33:38 +0200

    added base color that can be reverted to

--- core/src/io/piotrjastrzebski/ecsclones/restrainingofbob/components/rendering/DebugTint.java
+++ core/src/io/piotrjastrzebski/ecsclones/restrainingofbob/components/rendering/DebugTint.java
@@ -9,7 +9,25 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
  */
 public class DebugTint extends PooledComponent {
 	public Color color = new Color();
+	public Color base = new Color();
+
 	@Override protected void reset () {
 		color.set(Color.WHITE);
+	}
+
+	public DebugTint setBase (Color base) {
+		this.base.set(base);
+		color.set(base);
+		return this;
+	}
+
+	public DebugTint set (Color color) {
+		this.color.set(color);
+		return this;
+	}
+
+	public DebugTint setDdefault() {
+		color.set(base);
+		return this;
 	}
 }
