9816819427dc787b5995b9c506e56b3fb69bacff
PiotrJ <me@piotrjastrzebski.io>
2015-09-29 19:11:04 +0200

    added task that check if target is alive

---
+++ core/src/io/piotrjastrzebski/ecsclones/restrainingofbob/tasks/conditions/IsAliveTask.java
@@ -0,0 +1,42 @@
+package io.piotrjastrzebski.ecsclones.restrainingofbob.tasks.conditions;
+
+import com.artemis.ComponentMapper;
+import com.artemis.Entity;
+import com.artemis.annotations.Wire;
+import com.artemis.managers.TagManager;
+import com.badlogic.gdx.ai.btree.Task;
+import com.badlogic.gdx.ai.btree.annotation.TaskAttribute;
+import io.piotrjastrzebski.ecsclones.restrainingofbob.components.Dead;
+import io.piotrjastrzebski.ecsclones.restrainingofbob.components.logic.EnemyBrain;
+import io.piotrjastrzebski.ecsclones.restrainingofbob.tasks.base.BaseTask;
+
+/**
+ * Created by PiotrJ on 19/08/15.
+ */
+@Wire(injectInherited = true)
+public class IsAliveTask extends BaseTask {
+	private final static String TAG = IsAliveTask.class.getSimpleName();
+
+	@TaskAttribute(required=true)
+	public String target;
+
+	TagManager tags;
+	protected ComponentMapper<Dead> mDead;
+
+	@Override public void run () {
+		Entity te = tags.getEntity(target);
+		if (te == null || mDead.has(te)) {
+			fail();
+		} else {
+			success();
+		}
+	}
+
+	@Override protected Task<EnemyBrain> copyTo (Task<EnemyBrain> task) {
+		IsAliveTask range = (IsAliveTask)task;
+		range.target = target;
+		range.tags = tags;
+		range.mDead = mDead;
+		return super.copyTo(task);
+	}
+}
