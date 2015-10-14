7902b0001cf48f44d2a298b740a6fc3f6e123e55
PiotrJ <me@piotrjastrzebski.io>
2015-10-14 18:20:17 +0200

    wip btedit

--- core/src/io/piotrjastrzebski/ecsclones/restrainingofbob/utils/btedit/BTEditWindow.java
+++ core/src/io/piotrjastrzebski/ecsclones/restrainingofbob/utils/btedit/BTEditWindow.java
@@ -1,13 +1,18 @@
 package io.piotrjastrzebski.ecsclones.restrainingofbob.utils.btedit;
 
+import com.artemis.utils.reflect.ClassReflection;
+import com.artemis.utils.reflect.Field;
+import com.artemis.utils.reflect.ReflectionException;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ai.btree.BehaviorTree;
+import com.badlogic.gdx.ai.btree.BranchTask;
 import com.badlogic.gdx.ai.btree.Task;
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Tree;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.DragAndDrop;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ObjectMap;
 import com.badlogic.gdx.utils.Pool;
@@ -39,14 +44,33 @@ class BTEditWindow<E> extends VisWindow implements BehaviorTree.Listener<E> {
 
 	BehaviorTree bt;
 
+	DragAndDrop dad;
+
 	public BTEditWindow () {
 		super("BTEdit");
 		debugAll();
 		setResizable(true);
 		setSize(900, 1200);
+
+		dad = new DragAndDrop();
+
 		topTable = new VisTable(true);
 		trash = new VisLabel("Trash -> [_]");
 		topTable.add(trash);
+
+		dad.addTarget(new DragAndDrop.Target(trash) {
+			@Override public boolean drag (DragAndDrop.Source source, DragAndDrop.Payload payload, float x, float y, int pointer) {
+				// TODO check source if it can be trashed
+				return true;
+			}
+
+			@Override public void drop (DragAndDrop.Source source, DragAndDrop.Payload payload, float x, float y, int pointer) {
+				// remove node and stuff
+				TaskPayload tp = (TaskPayload)payload;
+				removeTaskNode(tp.getTaskNode());
+				Pools.free(tp);
+			}
+		});
 
 		treeTable = new VisTable(true);
 		editTable = new VisTable(true);
@@ -66,7 +90,53 @@ class BTEditWindow<E> extends VisWindow implements BehaviorTree.Listener<E> {
 		treeTable.add(treeView).expand().fill();
 	}
 
-	public void set (BehaviorTree bt) {
+	protected void removeTaskNode(TaskNode node) {
+		Task task = node.task;
+		// what do we do if we nuke the root?
+		if (bt == task) {
+			clearView();
+			return;
+		}
+		remove(bt, task);
+		// we need to reset as we modified internal structure
+		bt.reset();
+		// todo move this to free?
+		node.remove();
+		free(node);
+	}
+
+	private void remove(Task parent, Task toRemove) {
+//		int count = parent.getChildCount();
+		for (int i = 0; i < parent.getChildCount(); i++) {
+			Task child = parent.getChild(i);
+			if (child == toRemove) {
+				// how do we handle decorators? remove all to the bottom or just decorator?
+				if (!(parent instanceof BranchTask)) return;
+				// looks like we have no simple way of removing a thing...
+				// guess we need to rebuild the tree?
+				try {
+					Field field = ClassReflection.getDeclaredField(BranchTask.class, "children");
+					field.setAccessible(true);
+					Array<Task> children = (Array<Task>)field.get(parent);
+					children.removeValue(toRemove, true);
+					if (toRemove.getStatus() == Task.Status.RUNNING) {
+						toRemove.cancel();
+					}
+					// if it was last child, remove the parent a well
+//					if (children.size == 0) {
+//						removeTaskNode(taskToNode.get(parent));
+//					}
+					return;
+				} catch (ReflectionException e) {
+					e.printStackTrace();
+				}
+			} else {
+				remove(child, toRemove);
+			}
+		}
+	}
+
+	public void set (BehaviorTree<E> bt) {
 		if (bt != null) {
 			addView(bt);
 		} else {
@@ -100,11 +170,11 @@ class BTEditWindow<E> extends VisWindow implements BehaviorTree.Listener<E> {
 		taskToNode.put(task, node);
 		if (parent == null) {
 			// TODO some better name for root node
-			treeView.add(node.init(task));
+			treeView.add(node.init(task, dad));
 			node.fadeMin = 1;
 			node.label.setColor(COLOR_RUNNING);
 		} else {
-			parent.add(node.init(task));
+			parent.add(node.init(task, dad));
 		}
 		for (int i = 0; i < task.getChildCount(); i++) {
 			addViews(task.getChild(i), node);
@@ -151,7 +221,12 @@ class BTEditWindow<E> extends VisWindow implements BehaviorTree.Listener<E> {
 
 	ObjectMap<Task, TaskNode> taskToNode = new ObjectMap<>();
 	@Override public void statusUpdated (Task<E> task, Task.Status previousStatus) {
-		taskToNode.get(task).updateStatus(previousStatus);
+		TaskNode node = taskToNode.get(task, null);
+		if (node != null) {
+			node.updateStatus(previousStatus);
+		} else {
+			Gdx.app.log("", "Node not found for " + task);
+		}
 	}
 
 	public void childAdded(Task<E> task, int id) {
@@ -161,15 +236,28 @@ class BTEditWindow<E> extends VisWindow implements BehaviorTree.Listener<E> {
 	protected static class TaskNode extends VisTree.Node implements Pool.Poolable {
 		private VisLabel label;
 		private Task task;
-
+		private DragAndDrop dad;
+		private DragAndDrop.Source source;
 		public TaskNode () {
 			super(new VisLabel());
+			// tree uses object for comparison when removing nodes
+			setObject(this);
 			label = (VisLabel)getActor();
 		}
 
-		public TaskNode init(Task task) {
+		public TaskNode init (Task task, DragAndDrop dad) {
 			this.task = task;
+			this.dad = dad;
 			label.setText(task.getClass().getSimpleName());
+
+			source = new DragAndDrop.Source(label) {
+				@Override public DragAndDrop.Payload dragStart (InputEvent event, float x, float y, int pointer) {
+					TaskPayload payload = Pools.obtain(TaskPayload.class);
+					payload.init(label.getText().toString(), TaskNode.this);
+					return payload;
+				}
+			};
+			dad.addSource(source);
 			clearStatus();
 			return this;
 		}
@@ -188,6 +276,9 @@ class BTEditWindow<E> extends VisWindow implements BehaviorTree.Listener<E> {
 		@Override public void reset () {
 			task = null;
 			fade = 1;
+			dad.removeSource(source);
+			source = null;
+			dad = null;
 			clearStatus();
 			// free any children
 			Array<Tree.Node> children = getChildren();
@@ -231,6 +322,43 @@ class BTEditWindow<E> extends VisWindow implements BehaviorTree.Listener<E> {
 			default:
 				label.setColor(COLOR_NOT_RUN);
 			}
+		}
+	}
+
+	protected static class TaskPayload extends DragAndDrop.Payload implements Pool.Poolable {
+		private VisLabel drag;
+		private VisLabel valid;
+		private VisLabel invalid;
+		private TaskNode taskNode;
+
+		public TaskPayload () {
+			drag = new VisLabel();
+			drag.setColor(Color.WHITE);
+			setDragActor(drag);
+			valid = new VisLabel();
+			valid.setColor(Color.GREEN);
+			setValidDragActor(valid);
+			invalid = new VisLabel();
+			invalid.setColor(Color.RED);
+			setInvalidDragActor(invalid);
+		}
+
+		public void init (String text, TaskNode taskNode) {
+			drag.setText(text);
+			valid.setText(text);
+			invalid.setText(text);
+			this.taskNode = taskNode;
+		}
+
+		public TaskNode getTaskNode () {
+			return taskNode;
+		}
+
+		@Override public void reset () {
+			drag.setText("");
+			valid.setText("");
+			invalid.setText("");
+			taskNode = null;
 		}
 	}
 
